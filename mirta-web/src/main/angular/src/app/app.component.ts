815079a5ea8b8df40d398def042b225ee6e85017
Dmytro Popov <Dmytro.Popov@miratechgroup.com>
2017-12-24 20:55:35 +0200

    added login and registration pages

--- mirta-web/src/main/angular/src/app/app.component.ts
+++ mirta-web/src/main/angular/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
+import '../assets/app.css';
+
 @Component({
+  moduleId: module.id.toString(),
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
